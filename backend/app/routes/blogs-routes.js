071bc29ae96530d21ae0423547599ee74f20983c
Kavya Rachana Malluvalasa <malluvalasa.k@northeastern.edu>
2023-11-20 22:10:33 -0500

    Added routes to update and delete blogs in blogs-routes.js file

--- backend/app/routes/blogs-routes.js
+++ backend/app/routes/blogs-routes.js
@@ -1,11 +1,16 @@
 import express from 'express';
 
+import * as blogsController from '../controllers/blogs-controllers.js'; 
 
 const router = express.Router();
 
 router.route("/")
     // .get(blogsController.getBlogs)
     // .post(blogsController.saveBlog)
+
+router.route('/:id')
+    .put(blogsController.updateBlog)
+    .delete(blogsController.deleteBlog)
 
 
 export default router;
