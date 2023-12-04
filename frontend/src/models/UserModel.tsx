2de8baf6988ede7557e7318404d486bec2caf6f6
Sai Krishna Reddy Gaddam <gaddam.sai@northeastern.edu>
2023-12-03 19:50:22 -0500

    added User details

---
+++ frontend/src/models/UserModel.tsx
@@ -0,0 +1,10 @@
+interface User {
+    _id : String,
+    email : String,
+    firstName: String,
+    lastName: String,
+    profilePic: String,
+    role: String
+}
+
+export default User;
---
+++ frontend/src/services/UserService.tsx
@@ -0,0 +1,12 @@
+import User from "../models/UserModel";
+
+const user : User = {
+    _id : "656a9421c56c68861c127ac3",
+    email : "saikrishna1501@gmail.com",
+    firstName: "Sai",
+    lastName: "Gaddam",
+    profilePic: "https://icons8.com/icon/tZuAOUGm9AuS/user-default",
+    role: "student"
+}
+
+export default user;
