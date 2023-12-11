2253afbbec929b7ac368f28fe0124af26b70495a
Kavya Rachana Malluvalasa <malluvalasa.k@northeastern.edu>
2023-12-11 15:46:53 -0500

    Added housing reducer to entities

--- frontend/src/store/entities.ts
+++ frontend/src/store/entities.ts
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import blogsReducer from './blogs';
 import userReducer from './users';
 import eventReducer from './events'
+import housingReducer from './housing'
 
 export default combineReducers({
     blogs: blogsReducer,
     users: userReducer,
-    events: eventReducer
+    events: eventReducer,
+    housing: housingReducer
 })
