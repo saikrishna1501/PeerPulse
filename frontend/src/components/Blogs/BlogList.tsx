d908b1a7e687b9463e00b30226bd82ac63893cbc
Raveena Ingale <ingale.r@northeastern.edu>
2023-12-13 18:22:31 -0500

    Added logic for filters and search

--- frontend/src/components/Blogs/BlogList.tsx
+++ frontend/src/components/Blogs/BlogList.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import {
   Box,
+  Button,
+  Grid,
   IconButton,
   InputAdornment,
   Stack,
@@ -26,27 +28,82 @@ const findUser = (id: any, users: User[]) => {
   return user || placeholderUser;
 };
 
+const getThisWeek = () => {
+  const currentDate = new Date();
+  const firstDayOfWeek = new Date(
+    currentDate.setDate(currentDate.getDate() - currentDate.getDay())
+  );
+  return firstDayOfWeek.toISOString();
+};
+
+const getThisMonth = () => {
+  const currentDate = new Date();
+  const firstDayOfMonth = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth(),
+    1
+  );
+  return firstDayOfMonth.toISOString();
+};
+
+const getLastMonth = () => {
+  const currentDate = new Date();
+  const firstDayOfLastMonth = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() - 1,
+    1
+  );
+  return firstDayOfLastMonth.toISOString();
+};
+
 const BlogList: React.FC<Props> = ({ blogs, users }) => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [selectedFilter, setSelectedFilter] = useState<string | null>(null);
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
   };
 
-  // Filter blogs based on search term
-  const filteredBlogs = blogs.filter((blog) =>
-    blog.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const handleFilterClick = (filter: string) => {
+    setSelectedFilter(filter);
+  };
+
+  const filteredBlogs = blogs.filter((blog) => {
+    const lowercasedSearchTerm = searchTerm.toLowerCase();
+    const titleMatch = blog.title.toLowerCase().includes(lowercasedSearchTerm);
+    const tagMatch = blog?.tag?.some((tag) =>
+      tag.toLowerCase().includes(lowercasedSearchTerm)
+    );
+    const createdAt = new Date(blog.createdAt);
+
+    if (selectedFilter === "thisWeek") {
+      const thisWeekStart = new Date(getThisWeek());
+      return (titleMatch || tagMatch) && createdAt >= thisWeekStart;
+    } else if (selectedFilter === "thisMonth") {
+      const thisMonthStart = new Date(getThisMonth());
+      return (titleMatch || tagMatch) && createdAt >= thisMonthStart;
+    } else if (selectedFilter === "lastMonth") {
+      const lastMonthStart = new Date(getLastMonth());
+      const thisMonthStart = new Date(getThisMonth());
+      return (
+        (titleMatch || tagMatch) &&
+        createdAt >= lastMonthStart &&
+        createdAt < thisMonthStart
+      );
+    }
+
+    return titleMatch || tagMatch;
+  });
 
   return (
     <Box flex={4} p={2}>
-      <Stack>
+      <Stack direction="row" spacing={20}>
         <TextField
           onChange={handleSearch}
           id="standard-basic"
           variant="standard"
           placeholder="Search Blogs"
-          sx={{ paddingBottom: "20px", width: "700px", padding: "20px" }}
+          sx={{ paddingBottom: "20px", width: "400px", padding: "20px" }}
           InputProps={{
             startAdornment: (
               <InputAdornment position="start">
@@ -57,6 +114,42 @@ const BlogList: React.FC<Props> = ({ blogs, users }) => {
             ),
           }}
         />
+        <Stack direction={"row"} sx={{ display: "flex", alignItems: "center" }}>
+          <Grid sx={{ marginRight: "10px" }}>
+            <Button
+              sx={{
+                backgroundColor:
+                  selectedFilter === "thisWeek"
+                    ? "rgba(0, 0, 0, 0.2)"
+                    : "rgba(0, 0, 0, 0.08)",
+                width: "100px",
+                borderRadius: "10px",
+                color: "black",
+              }}
+              color="primary"
+              onClick={() => handleFilterClick("thisWeek")}
+            >
+              This week
+            </Button>
+          </Grid>
+          <Grid>
+            <Button
+              sx={{
+                backgroundColor:
+                  selectedFilter === "lastMonth"
+                    ? "rgba(0, 0, 0, 0.2)"
+                    : "rgba(0, 0, 0, 0.08)",
+                width: "100px",
+                borderRadius: "10px",
+                color: "black",
+              }}
+              color="primary"
+              onClick={() => handleFilterClick("lastMonth")}
+            >
+              Last month
+            </Button>
+          </Grid>
+        </Stack>
       </Stack>
       <br />
       {filteredBlogs.map((blog) => (
