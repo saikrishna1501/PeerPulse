e021a6731b04c28e7c6961eb0d8406ec76c455f6
Raveena Ingale <ingale.r@northeastern.edu>
2023-12-13 16:19:16 -0500

    fix:Login and link

--- frontend/src/components/auth/LoginRequiredPage.tsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-import  {AUTH_ROUTE}  from "../../constants/routes";
-
-export const LoginRequiredPage = () => (
-    <div>
-      <p>To view this content, please log in.</p>
-      <Link to={AUTH_ROUTE}>Go to Login Page</Link>
-    </div>
-  );
