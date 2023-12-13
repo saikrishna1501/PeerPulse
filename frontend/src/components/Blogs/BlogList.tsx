b520951bf02e7e00b9e00560017473235d158bf4
Raveena Ingale <ingale.r@northeastern.edu>
2023-12-13 17:31:46 -0500

    fix:SearchBlogsdone

--- frontend/src/components/Blogs/BlogList.tsx
+++ frontend/src/components/Blogs/BlogList.tsx
@@ -1,4 +1,12 @@
-import { Box } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Box,
+  IconButton,
+  InputAdornment,
+  Stack,
+  TextField,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 import BlogCard from "./BlogCard";
 import Blog from "../../models/blogs";
 import User from "../../models/users";
@@ -14,19 +22,47 @@ const findUser = (id: any, users: User[]) => {
     lastName: "Ing",
     profilePic: "",
   } as User;
-  const user = users.find((user) => user._id == id);
-  return user ? user : placeholderUser;
+  const user = users.find((user) => user._id === id);
+  return user || placeholderUser;
 };
 
-const BlogList = ({ blogs, users }: Props) => {
+const BlogList: React.FC<Props> = ({ blogs, users }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  // Filter blogs based on search term
+  const filteredBlogs = blogs.filter((blog) =>
+    blog.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <>
-      <Box flex={4} p={2}>
-        {blogs.map((blog) => (
-          <BlogCard blog={blog} author={findUser(blog.author, users)} />
-        ))}
-      </Box>
-    </>
+    <Box flex={4} p={2}>
+      <Stack>
+        <TextField
+          onChange={handleSearch}
+          id="standard-basic"
+          variant="standard"
+          placeholder="Search Blogs"
+          sx={{ paddingBottom: "20px", width: "700px", padding: "20px" }}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <IconButton>
+                  <SearchIcon />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Stack>
+      <br />
+      {filteredBlogs.map((blog) => (
+        <BlogCard blog={blog} author={findUser(blog.author, users)} />
+      ))}
+    </Box>
   );
 };
 
