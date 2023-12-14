caa9f863a72b87002aea2d86f0ccb058aef7c8d1
Hari Sriya Pelluri <pelluri.h@northeastern.edu>
2023-12-13 23:07:03 -0500

    Added condition to hide register button for logged in users

--- frontend/src/components/Button/SignupButton.tsx
+++ frontend/src/components/Button/SignupButton.tsx
@@ -1,11 +1,18 @@
 import { useNavigate } from "react-router-dom";
 import MyButton from "./Button";
 import { REGISTER_ROUTE } from "../../constants/routes";
+import { useSelector } from "react-redux";
 
 const SignupButton = () => {
   const navigate = useNavigate();
+  const isAuthenticated = useSelector(
+    (state: any) => state.auth.isAuthenticated
+  );
 
   const navigateTo = (route: string) => () => navigate(route);
-  return <MyButton text={"Get Started"} url={REGISTER_ROUTE} isArrow={true} />;
+  if(!isAuthenticated){
+    return <MyButton text={"Get Started"} url={REGISTER_ROUTE} isArrow={true} />
+  }
+  return (<></>)
 };
 export default SignupButton;
